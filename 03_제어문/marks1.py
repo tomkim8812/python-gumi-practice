d0988beca5a9556e82210bca474dd723047fa5b1
tomkim8812 <kch5317@gmail.com>
2024-08-19 11:40:13 +0900

    20240819

---
+++ 03_제어문/marks1.py
@@ -0,0 +1,8 @@
+marks=[90,25,67,45,80]
+number=0
+for mark in marks:
+    number=number+1
+    if mark >=60:
+        print(f'{number}번 학생은 합격입니다.')
+    else:
+        print(f'{number}번 학생은 불합격입니다.')
